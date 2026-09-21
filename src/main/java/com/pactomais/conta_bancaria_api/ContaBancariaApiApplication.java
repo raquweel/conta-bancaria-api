38c1cbd9c537ebe1b21e50ea713b711f5a8fd615
Raquel <anaraquelbpereira@gmail.com>
2026-09-21 18:21:09 -0300

    configurações e estruturação do projeto

---
+++ src/main/java/com/pactomais/conta_bancaria_api/ContaBancariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.pactomais.conta_bancaria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pactomais/conta_bancaria_api/ContaBancariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pactomais.conta_bancaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
